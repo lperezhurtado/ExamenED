788fd718158552f32aaa8d0979419c758d118697
luis <lperezhurtado97@gmail.com>
2021-12-01 17:33:22 +0100

    Reto 2 completado

---
+++ ExamenED/src/examened/Reto2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examened;
+
+/**
+ *
+ * @author examened
+ */
+public class Reto2 {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 1000; i++) {
+            
+            System.out.println(i);
+            
+        }
+    }
+    
+    
+}
